0cf0b10ec2f1ab4934350e075b95d962a72aa890
Madeline Bennett <madeline.bennett.pm@gmail.com>
2024-10-17 22:16:41 -0700

    book model added

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,6 @@
 from flask import Flask
-from .routes.hello_world_routes import hello_world_bp
+from .models.book import book_bp
 def create_app():
     app = Flask(__name__)
-    app.register_blueprint(hello_world_bp)
-
+    app.register_blueprint(book_bp)
     return app
---
+++ app/models/book.py
@@ -0,0 +1,28 @@
+from flask import Blueprint
+import json
+class Book:
+    def __init__(self, id, title, description):
+        self.id = id
+        self.title = title
+        self.description = description
+
+books = [
+    Book(1, "Fictional Book", "A fantasy novel set in an imaginary world."),
+    Book(2, "Wheel of Time", "A fantasy novel set in an imaginary world."),
+    Book(3, "Fictional Book Title", "A fantasy novel set in an imaginary world")
+]
+
+book_bp = Blueprint("book",__name__)
+
+@book_bp.get("/books")
+def print_books_info():
+    dictionary_list = []
+    for book in books: 
+        book_dictionary = {
+            "id" : book.id ,
+            "title" : book.title,
+            "description" : book.description
+        }
+        dictionary_list.append(book_dictionary)
+    return dictionary_list
+
